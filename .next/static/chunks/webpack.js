b0466a5e484012794383f771ad6c2d6416f46a71
devsadikb <devsadikbd@gmail.com>
2025-07-31 20:52:27 +0600

    Add hot-update files for webpack with updated hashes and error handling
    - Created new hot-update JSON files for webpack, including updates for pages/_error and orders.
    - Added corresponding hot-update JS files with eval-source-map devtool for better debugging.
    - Updated hash values for various webpack runtime modules to reflect recent changes.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "26bffaf33e297e3c701a"; }
+/******/ 		__webpack_require__.h = function() { return "f2abf76f992f39bcdfbe"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
