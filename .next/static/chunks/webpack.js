d417c23ada92d3d3cee3e8d242bec093afe84cf0
devsadikb <devsadikbd@gmail.com>
2025-07-31 14:09:08 +0600

    Refactor paginationField.js and update webpack hot-update files
    - Implemented a new PaginationField function in paginationField.js with improved read method to log existing data and args.
    - Updated multiple webpack hot-update files to reflect changes in the build process and ensure compatibility with the latest code structure.
    - Enhanced source maps for better debugging during development.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d99e9e31392afb860d41"; }
+/******/ 		__webpack_require__.h = function() { return "a9655c772e40cbcaf15b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
